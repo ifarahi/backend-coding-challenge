c1581df1c3c9d02706fbe241157d20ed5b0dd933
Ismail Farahi <dr0farahi@gmail.com>
2020-01-28 00:25:50 +0100

    Complete repositories controller

--- app/controllers/repositories.js
+++ app/controllers/repositories.js
@@ -1,4 +1,32 @@
-const axios = require('axios');
+const axios = require("axios");
+const getLanguageInfo = require('../helpers/getLanguageInfo');
 
-exports.languages = async (req, res) => {
-}
+exports.languages = async (req, res, next) => {
+    try {
+        const { data } = await axios.get("https://github-trending-api.now.sh/");
+        const languages = new Set();
+        const responseObject = {
+            count: 0,
+            languages: []
+        };
+
+        // exports all the languages from the repositories
+        data.forEach(repo => {
+            if (repo.language) {
+                languages.add(repo.language);
+            }
+        });
+
+        // get needed informations from every language
+        languages.forEach(language => {
+            responseObject.languages.push(getLanguageInfo(data, language));
+        });
+
+        // include the languages number
+        responseObject.count = responseObject.languages.length;
+
+        res.status(200).json(responseObject);
+    } catch (error) {
+        next(error);
+    }
+};
